311723d2a18d253a5309f95737fbcd2656fae5ef
Ariel Ahdoot <arieli612@aol.com>
2015-10-29 18:14:15 -0400

    indexer.h

---
+++ indexer.h
@@ -0,0 +1,27 @@
+#ifndef INDEXER_H
+#define INDEXER_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+typedef char* String;
+typedef struct dirent* EntryPtr;
+typedef DIR* Dirp;
+
+ typedef struct Occurrence{
+ 	int freq;
+ 	String filename;
+ }Occurrence;
+
+
+
+
+
+
+
+#endif
